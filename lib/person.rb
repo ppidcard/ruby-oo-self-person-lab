47b454fbf8de53cb245bfc4f0642c6ba0e672062
ppidcard <tvxqlikei@live.cn>
2021-07-13 18:41:34 +1000

    done

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,70 @@
 # your code goes here
+
+class Person
+
+    def initialize(name)
+        @name = name
+        @bank_account = 25
+        @happiness = 8
+        @hygiene = 8
+    end
+
+    attr_reader :name
+    attr_accessor :bank_account, :hygiene, :happiness
+    
+    def happiness
+        @happiness
+    end
+
+    def happiness=(score)
+        @happiness = score.clamp(0, 10)
+
+    end
+
+    def hygiene=(score)
+        @hygiene = score.clamp(0, 10)
+    end
+
+    def happy?
+        @happiness > 7
+    end
+
+    def clean?
+        @hygiene > 7
+    end
+
+    def get_paid(salary)
+        self.bank_account += salary
+        "all about the benjamins"
+    end
+
+    def take_bath
+        self.hygiene += 4    
+        '♪ Rub-a-dub just relaxing in the tub ♫'
+    end
+
+    def work_out
+        self.hygiene -= 3
+        self.happiness += 2
+        '♪ another one bites the dust ♫'
+    end
+
+    def call_friend(friend)
+        self.happiness += 3
+        friend.happiness += 3
+        "Hi #{friend.name}! It's #{self.name}. How are you?"
+    end
+
+    def start_conversation(friend, topic)
+        if topic == 'politics'
+            self.happiness -= 2
+            friend.happiness -= 2
+           return  'blah blah partisan blah lobbyist'
+        elsif topic == 'weather'
+            self.happiness += 1
+            friend.happiness += 1
+           return 'blah blah sun blah rain'
+        else return 'blah blah blah blah blah'
+    end
+end
+end
